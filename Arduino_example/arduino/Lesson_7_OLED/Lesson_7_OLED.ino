12f44df7498615230fdbaf53e90601dc5cff1165
a80601pass <39110721+a80601pass@users.noreply.github.com>
2022-08-13 16:02:56 +0800

    Update Lesson_7_OLED.ino

--- Arduino_example/arduino/Lesson_7_OLED/Lesson_7_OLED.ino
+++ Arduino_example/arduino/Lesson_7_OLED/Lesson_7_OLED.ino
@@ -8,6 +8,6 @@ void setup() {
 
 void loop() {
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.setCursor(0, 0);
-  u8x8.print("Hello World!");
+  u8x8.setCursor(0, 0);//更改座標數值可改變文字位置
+  u8x8.print("Hello World!");//更改冒號內文字可改變顯示內容
 }
